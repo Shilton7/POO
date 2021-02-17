76973c4b46442d89afc1fb5f05d12d053763b3cf
SHILTON PEREIRA SANTIAGO - 900000968 <shilton.santiago@animaeducacao.com.br>
2021-02-17 11:15:54 -0300

    Business rules for the career item

--- Balta/Program.cs
+++ Balta/Program.cs
@@ -22,14 +22,16 @@ namespace Balta
             }
 
             var course = new List<Course>();
-            course.Add(new Course("Curso de Docker 1", "docker.com"));
-            course.Add(new Course("Curso de Docker 2", "docker.com"));
-            course.Add(new Course("Curso de SQL SERVER", "mssql.com"));
+            var courseNet = new Course("Curso de .NET 1", "net.com");
+            var courseSQL = new Course("Curso de SQL SERVER", "mssql.com");
+            course.Add(courseNet);
+            course.Add(courseSQL);
+
 
             var career = new Career("Carreira .NET","net.com");
-            var careerItem = new CareerItem(1,"Comece por aqui","O start dessa carreira é show",null);
-            var careerItem3 = new CareerItem(3, "Terceiro passo por aqui", "O passo 3 dessa carreira é show", null);
-            var careerItem2 = new CareerItem(2, "Segundo passo por aqui", "O passo 2 dessa carreira é show", null);
+            var careerItem = new CareerItem(1,"Comece por aqui","O start dessa carreira é show", courseNet);
+            var careerItem3 = new CareerItem(3, "Terceiro passo por aqui", "O passo 3 dessa carreira é show", courseSQL);
+            var careerItem2 = new CareerItem(2, "Segundo passo por aqui", "O passo 2 dessa carreira é show", courseNet);
             career.Items.Add(careerItem);
             career.Items.Add(careerItem3);
             career.Items.Add(careerItem2);
@@ -37,7 +39,7 @@ namespace Balta
             foreach(var item in career.Items.OrderBy(x=> x.Order))
             {
                 Console.WriteLine($"{item.Order} - {item.Title} - {item.Description}");
-                Console.WriteLine(item.Course);
+                Console.WriteLine(item.Course.Title);
             }
 
         }
