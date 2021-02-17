87298c3d1b90f70d5c8bc7c3ebcacde23b3fe11c
SHILTON PEREIRA SANTIAGO - 900000968 <shilton.santiago@animaeducacao.com.br>
2021-02-17 11:44:18 -0300

    Notifications and Inheritance

--- Balta/ContentContext/Content.cs
+++ Balta/ContentContext/Content.cs
@@ -4,16 +4,14 @@ using System.Text;
 
 namespace Balta.ContentContext
 {
-    public abstract class Content
+    public abstract class Content : Base
     {
         public Content(string title, string url)
         {
-            Id = Guid.NewGuid();
             Title = title;
             Url = url;
         }
-
-        public Guid Id { get; set; }
+        
         public string Title { get; set; }
         public string Url { get; set; }
     }
