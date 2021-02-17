2e52cb884dbb33bf13d2728f72e7f934aea99f71
SHILTON PEREIRA SANTIAGO - 900000968 <shilton.santiago@animaeducacao.com.br>
2021-02-17 12:13:01 -0300

    Subscription context

---
+++ Balta/SubscriptionContext/User.cs
@@ -0,0 +1,11 @@
+﻿using Balta.SharedContext;
+
+namespace Balta.SubscriptionContext
+{
+    public class User : Base
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Active { get; set; }
+    }
+}
